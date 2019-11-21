3b3a18f4a75156cc67145f739c3414c0c4b39655
dzimiks <vana997@gmail.com>
2019-11-21 14:55:40 +0100

    Make some progression

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -8,30 +8,33 @@ if __name__ == '__main__':
     successful = 0
     MAIN_URL = 'https://www.imdb.com'
 
-    for i in range(1, 2, 100):
+    for i in range(1, 302, 100):
         URL = f'https://www.imdb.com/search/title/?title_type=feature,tv_series&count=100&start={i}&ref_=adv_nxt'
         page = requests.get(URL).text
         soup = BeautifulSoup(page, features='lxml')
         soup.prettify()
 
-        test = [re.sub(r'<[^>]*>', '', item[0]).strip() for item in re.findall(r'Stars:((.|\n)*?)</a>', page)]
-        test2 = re.findall(r'Stars:((.|\n)*?)</a>', page)
-        print(test)
-        print(test2)
-
         all_movies = soup.find_all('div', 'lister-item mode-advanced')
+        j = 0
 
         for movie in all_movies:
             item_content = movie.find('div', 'lister-item-content')
             item_header = item_content.find('h3', 'lister-item-header').text
             item_header_split = list(filter(None, item_header.split('\n')))
-            item_subtitle = [item.text for item in item_content.find_all('p')]
+            item_subtitle = [item.text.strip() for item in item_content.find_all('p')]
+            item_subtitle = [item.split('|') for item in item_subtitle]
 
-            url = MAIN_URL + item_content.find('h3', 'lister-item-header').find('a').get('href')
-            # print('url:', url)
+            item_subtitle_len = len(item_subtitle)
+            duration_and_genre = None
+            bio = None
+            director_and_stars = None
+            votes = None
 
-            item_content_trimmed = re.sub(r'\n+', r'\n', item_content.text)
-            item_content_trimmed = re.sub(r' +', ' ', item_content_trimmed)
+            if item_subtitle_len == 4:
+                duration_and_genre = [item.strip() for item in item_subtitle[0]]
+                bio = item_subtitle[1][0].strip()
+                director_and_stars = [item.strip() for item in item_subtitle[2]]
+                votes = [item.strip() for item in item_subtitle[3]]
 
             # certificate = item_subtitle[0].find('span', 'certificate')
             # duration = item_subtitle[0].find('span', 'runtime')
@@ -44,20 +47,24 @@ if __name__ == '__main__':
             # votes = item_subtitle
 
             movie = {
-                'header': item_header,
+                'header': item_header_split,
                 'item_subtitle': item_subtitle,
-                'len': len(item_subtitle),
-                'item_content_trimmed': item_content_trimmed
-                # 'bio': bio,
-                # 'director': director,
-                # 'stars': stars,
-                # 'votes': votes
+                'duration_and_genre': duration_and_genre,
+                'bio': bio,
+                'director_and_stars': director_and_stars,
+                'votes': votes
             }
 
             movies.append(movie)
             successful += 1
             # print(json.dumps(movie, default=str, indent=4))
 
+            # if j == 2:
+            #     break
+
+            j += 1
+
     print('Successfully found:', successful)
-    # with open('data.json', 'w', encoding='utf-8') as f:
-    #     json.dump(movies, f, ensure_ascii=False, indent=4)
+
+    with open('data.json', 'w+', encoding='utf-8') as f:
+        json.dump(movies, f, ensure_ascii=False, indent=4)
