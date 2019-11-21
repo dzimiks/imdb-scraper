9a5802cf111acebb39bbc4a907ec1f4ab1b0abe4
dzimiks <vana997@gmail.com>
2019-11-21 18:51:03 +0100

    Add user reviews

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -6,16 +6,16 @@ if __name__ == '__main__':
     movies = []
     successful = 0
     MAIN_URL = 'https://www.imdb.com'
+    REVIEWS_SUFFIX = 'reviews?sort=submissionDate&dir=desc&ratingFilter=0'
 
-    # TODO: Retrieve 11100 movies
-    for i in range(1, 11002, 100):
+    # TODO: Retrieve 11100 movies - 11002
+    for i in range(1, 101, 100):
         URL = f'https://www.imdb.com/search/title/?title_type=feature,tv_series&count=100&start={i}&ref_=adv_nxt'
         page = requests.get(URL).text
         soup = BeautifulSoup(page, features='lxml')
         soup.prettify()
 
         all_movies = soup.find_all('div', 'lister-item mode-advanced')
-        # j = 0
 
         for movie in all_movies:
             item_content = movie.find('div', 'lister-item-content')
@@ -23,6 +23,8 @@ if __name__ == '__main__':
             item_header_split = list(filter(None, item_header.split('\n')))
             item_subtitle = [item.text.strip() for item in item_content.find_all('p')]
             item_subtitle = [item.split('|') for item in item_subtitle]
+            movie_url = MAIN_URL + item_content.find('h3', 'lister-item-header').find('a').get('href')
+            movie_reviews_url = movie_url + REVIEWS_SUFFIX
 
             # TODO: 257. After we Collided without movie year
             item_header_split_len = len(item_header_split)
@@ -37,6 +39,9 @@ if __name__ == '__main__':
             elif item_header_split_len == 2:
                 movie_id = item_header_split[0]
                 movie_title = item_header_split[1]
+
+            if movie_id is not None:
+                movie_id = int(movie_id.replace('.', ''))
 
             # Movie part is in year
             if movie_year is not None and ') (' in movie_year:
@@ -60,7 +65,7 @@ if __name__ == '__main__':
                 director_and_stars = [item.strip() for item in item_subtitle[2]]
 
             if votes is not None:
-                votes = votes[0].replace('Votes:', '').strip()
+                votes = int(votes[0].replace('Votes:', '').replace(',', '').strip())
 
             duration_and_genre_len = len(duration_and_genre)
             certificate = None
@@ -69,7 +74,7 @@ if __name__ == '__main__':
 
             if duration_and_genre_len == 3 or duration_and_genre_len == 4:
                 # Post production
-                if duration_and_genre[0][0].isdigit():
+                if 'min' in duration_and_genre[0]:
                     duration = duration_and_genre[0]
                     genre = duration_and_genre[1]
                 else:
@@ -85,6 +90,12 @@ if __name__ == '__main__':
                     genre = duration_and_genre[0]
             elif duration_and_genre_len == 1:
                 genre = duration_and_genre[0]
+
+            if duration is not None:
+                duration = int(duration.replace(' min', ''))
+
+            if genre is not None:
+                genre = [item.strip() for item in genre.split(',')]
 
             director_and_stars_len = len(director_and_stars)
             director = None
@@ -107,34 +118,52 @@ if __name__ == '__main__':
             rating = item_content.find('div', 'ratings-imdb-rating')
 
             if rating is not None:
-                rating = rating.text.strip()
+                rating = float(rating.text.strip())
+
+            current_reviews = requests.get(movie_reviews_url).text
+            soup_reviews = BeautifulSoup(current_reviews, features='lxml')
+            soup_reviews.prettify()
+
+            all_reviews = soup_reviews.find_all('div', 'review-container')
+            movie_reviews = []
+
+            # TODO: Iterate over all reviews from movie page
+            for review in all_reviews:
+                review_rating = review.find('span', 'rating-other-user-rating')
+
+                if review_rating is not None:
+                    review_rating = int(review_rating.text.split('/')[0].strip())
+
+                review_title = review.find('a', 'title')
+
+                if review_title is not None:
+                    review_title = review_title.text.strip()
+
+                movie_reviews.append({
+                    'review_rating': review_rating,
+                    'review_title': review_title
+                })
 
             movie = {
-                # 'header': item_header_split,
                 'movie_id': movie_id,
                 'movie_title': movie_title,
                 'movie_year': movie_year,
-                # 'item_subtitle': item_subtitle,
-                # 'duration_and_genre': duration_and_genre,
+                'movie_url': movie_url,
+                'movie_reviews_url': movie_reviews_url,
                 'rating': rating,
                 'bio': bio,
-                # 'director_and_stars': director_and_stars,
                 'director': director,
                 'stars': stars,
                 'votes': votes,
                 'certificate': certificate,
                 'duration': duration,
-                'genre': genre
+                'genre': genre,
+                'movie_reviews': movie_reviews
             }
 
             movies.append(movie)
             successful += 1
             # print(json.dumps(movie, default=str, indent=4))
-
-            # if j == 2:
-            #     break
-
-            # j += 1
 
     print('Successfully found:', successful)
 
